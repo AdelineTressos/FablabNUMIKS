c7f8a0be545e2c8610f1e1a444ecff423c8088da
AdelineTressos <147603609+AdelineTressos@users.noreply.github.com>
2024-08-20 14:52:21 +0200

    Add files via upload

---
+++ app/Service/FileUploader.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileUploader
+{
+    private string $targetDirectory;
+
+    public function __construct(string $targetDirectory)
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function upload(UploadedFile $file): string
+    {
+        $fileName = uniqid() . '.' . $file->guessExtension();
+        $file->move($this->getTargetDirectory(), $fileName);
+
+        return $fileName;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
+    }
+
+    public function remove($filename)
+    {
+        $filePath = $this->targetDirectory . '/' . $filename;
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+    }
+}
